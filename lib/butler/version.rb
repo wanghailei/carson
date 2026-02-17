2b8f923a1818a92755df3ae9395cbc19457dc035
WHL <wanghailei@gmail.com>
2026-02-17 19:30:24 +0800

    chore(ruby): remove frozen string literal pragmas (#18)

--- lib/butler/version.rb
+++ lib/butler/version.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module Butler
 	version_path = File.expand_path( "../../VERSION", __dir__ )
 	VERSION = File.file?( version_path ) ? File.read( version_path ).strip : "0.0.0"
